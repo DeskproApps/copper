1c4c05f4cbaaf4a901dc4b3ffdfed48f011104bc
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-08-21 11:49:34 +0100

    [Feature] SC-124017 Refactor apps to remove iframe-resizer dependency and make iframe content sizing more robust

--- src/main.tsx
+++ src/main.tsx
@@ -10,30 +10,33 @@ import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "./query";
 import { App } from "./App";
 import { ErrorFallback } from "./components/ErrorFallback/ErrorFallback";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "flatpickr/dist/themes/light.css";
 import "simplebar/dist/simplebar.min.css";
 import "tippy.js/dist/tippy.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
+import "./main.css";
+import { Scrollbar } from "@deskpro/deskpro-ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
-    <DeskproAppProvider>
-      <HashRouter>
-        <QueryClientProvider client={queryClient}>
-          <Suspense fallback={<LoadingSpinner/>}>
-            <QueryErrorResetBoundary>
-              {({ reset }) => (
-                <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
-                  <App/>
-                </ErrorBoundary>
-              )}
-            </QueryErrorResetBoundary>
-          </Suspense>
-        </QueryClientProvider>
-      </HashRouter>
-    </DeskproAppProvider>
+    <Scrollbar style={{ height: "100%", width: "100%" }}>
+      <DeskproAppProvider>
+        <HashRouter>
+          <QueryClientProvider client={queryClient}>
+            <Suspense fallback={<LoadingSpinner/>}>
+              <QueryErrorResetBoundary>
+                {({ reset }) => (
+                  <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
+                    <App/>
+                  </ErrorBoundary>
+                )}
+              </QueryErrorResetBoundary>
+            </Suspense>
+          </QueryClientProvider>
+        </HashRouter>
+      </DeskproAppProvider>
+    </Scrollbar>
   </StrictMode>
 );
