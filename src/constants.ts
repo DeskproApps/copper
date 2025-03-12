54be6828071344957a8f164b57006be3fa5ca238
OJ Abba <phavorabba@gmail.com>
2025-03-12 12:07:20 +0000

    [Feat] Add OAuth constants

--- src/constants.ts
+++ src/constants.ts
@@ -12,6 +12,8 @@ export const ENTITY = "linkedCopperContact";
 export const placeholders = {
   API_KEY: "__api_key__",
   API_OWNER_EMAIL: "__api_key_owner_email__",
+  OAUTH2_ACCESS_TOKEN_PATH: "oauth2/access_token",
+  OAUTH2_REFRESH_TOKEN_PATH: "oauth2/refresh_token"
 };
 
 /** Copper */
